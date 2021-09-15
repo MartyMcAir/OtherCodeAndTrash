efc30b0ec5b7ee1f213848920d5ab2fd2835ef7e
MartyMcAir <martymcair@gmail.com>
2021-09-15 05:14:24 +0300

    sitePoint_com added

--- MyTrash/src/main/java/GUI_SingleFiles/Utf8_JFrameWindowListener.java
+++ MyTrash/src/main/java/GUI_SingleFiles/Utf8_JFrameWindowListener.java
@@ -6,45 +6,56 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 // http://java-online.ru/swing-windows.xhtml
-public class Utf8_JFrameWindowListener extends JFrame
-{
-	private static final long serialVersionUID = 1L;
-	private static int    counter = 0; // СЃС‡РµС‚С‡РёРє
-	private static JLabel label   = null;
-	private static String TEMPL   = "Р—Р°РєСЂС‹С‚РёРµ РѕРєРЅР° (РїРѕРїС‹С‚РѕРє %d)";
-	public Utf8_JFrameWindowListener ()
-	{
-		// РЎРѕР·РґР°РЅРёРµ РѕРєРЅР° СЃ Р·Р°РіРѕР»РѕРІРєРѕРј
+public class Utf8_JFrameWindowListener extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private static int counter = 0; // счетчик
+    private static JLabel label = null;
+    private static String TEMPL = "Закрытие окна (попыток %d)";
+
+    public Utf8_JFrameWindowListener() {
+        // Создание окна с заголовком
         JFrame frame = new JFrame("JFrameWindowListener");
-		// РќРµ Р·Р°РєСЂС‹РІР°С‚СЊ РѕРєРЅРѕ РїРѕ РЅР°Р¶Р°С‚РёСЋ РЅР° РєРЅРѕРїРєСѓ СЃ РєСЂРµСЃС‚РёРєРѕРј 
+        // Не закрывать окно по нажатию на кнопку с крестиком
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        // РџРѕРґРєР»СЋС‡РµРЅРёРµ СЃР»СѓС€Р°С‚РµР»СЏ РѕРєРЅР°
+        // Подключение слушателя окна
         frame.addWindowListener(new WindowListener() {
- 
-            public void windowActivated  (WindowEvent event) {}
-            public void windowClosed     (WindowEvent event) {}
-            public void windowDeactivated(WindowEvent event) {}
-            public void windowDeiconified(WindowEvent event) {}
-            public void windowIconified  (WindowEvent event) {}
-            public void windowOpened     (WindowEvent event) {}
-            // РњРµС‚РѕРґ РѕР±СЂР°Р±РѕС‚РєРё СЃРѕР±С‹С‚РёСЏ "Р—Р°РєСЂС‹С‚РёРµ РѕРєРЅР°"
-            public void windowClosing (WindowEvent event) {
-            	if (++counter == 3) {
-            		event.getWindow().setVisible(false);
-            		System.exit(0);
-            	} else
-            		label.setText(String.format(TEMPL, counter));
+
+            public void windowActivated(WindowEvent event) {
             }
-        }); 
+
+            public void windowClosed(WindowEvent event) {
+            }
+
+            public void windowDeactivated(WindowEvent event) {
+            }
+
+            public void windowDeiconified(WindowEvent event) {
+            }
+
+            public void windowIconified(WindowEvent event) {
+            }
+
+            public void windowOpened(WindowEvent event) {
+            }
+
+            // Метод обработки события "Закрытие окна"
+            public void windowClosing(WindowEvent event) {
+                if (++counter == 3) {
+                    event.getWindow().setVisible(false);
+                    System.exit(0);
+                } else
+                    label.setText(String.format(TEMPL, counter));
+            }
+        });
         label = new JLabel(String.format(TEMPL, counter));
         frame.getContentPane().add(label);
 
         frame.setPreferredSize(new Dimension(250, 80));
         frame.pack();
         frame.setVisible(true);
-	}
-    public static void main(String[] args)
-    {
+    }
+
+    public static void main(String[] args) {
         JFrame.setDefaultLookAndFeelDecorated(true);
         new Utf8_JFrameWindowListener();
     }
---
+++ OtherTrash/src/main/java/sitepoint_com/Tmp.java
@@ -0,0 +1,5 @@
+package sitepoint_com;
+
+// https://www.sitepoint.com/ultimate-guide-to-java-9/
+public class Tmp {
+}
